c584ce84a247f2702c3372f742b4cb14178a6806
Yhennon <loostomi@windowslive.com>
2021-01-06 12:43:29 +0100

    Created a class for constant values

---
+++ app/src/main/java/com/example/booklist/Constants.java
@@ -0,0 +1,8 @@
+package com.example.booklist;
+
+public class Constants {
+
+    public static final String SHOWX = "SHOWX";
+    public static final String DATABASE_NAME = "books_db";
+    public static final int DATABASE_VERSION = 1;
+}
